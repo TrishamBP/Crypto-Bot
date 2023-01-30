d89a99884544e88e1a2ae3048362329bffac0776
Trisham <trishampatil@gmail.com>
2023-01-30 06:05:26 +0530

    Keys and secret for each CEX stored in config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 // Imports
 const ccxt = require("ccxt"); // Importing ccxt to connect with CEX
+const config = require("../config/config.json");
 require("dotenv").config(); // To read values from .env file
-// Importing coinbase parameters from .env
-const coinbaseapiKEY = process.env.COINBASE_API_KEY;
-const coinbaseSecret = process.env.COINBASE_API_SECRET;
+// Importing coinbase parameters from config
+const coinbaseapiKEY = config.Coinbase.API_KEY;
+const coinbaseSecret = config.Coinbase.API_SECRET;
+// Initiating ccxt connection to coinbase
+const exchangeId = "coinbase",
+  exchangeClass = ccxt[exchangeId],
+  exchange = new exchangeClass({
+    apiKey: coinbaseapiKEY,
+    secret: coinbaseSecret,
+    rateLimit: true, // Set the rate limit for how fast you want to get the data
+    verbose: true,
+  });
+// Fetching all balances
 async function fetchAllBalances(exchange) {
   const params = {};
   let balance = {};
@@ -25,16 +36,7 @@ async function fetchAllBalances(exchange) {
   return balance;
 }
 async function main() {
-  const exchangeId = "coinbase",
-    exchangeClass = ccxt[exchangeId],
-    exchange = new exchangeClass({
-      apiKey: coinbaseapiKEY,
-      secret: coinbaseSecret,
-      rateLimit: true, // Set the rate limit for how fast you want to get the data
-      verbose: true,
-    });
   const markets = await exchange.loadMarkets();
-  // coinbase.verbose = true // uncomment for debugging purposes if necessary
   const balance = await fetchAllBalances(exchange);
   return balance;
 }
